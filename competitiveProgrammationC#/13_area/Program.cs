9b7cdc03eeb43a5a2b5ffa1e6a05ed12a154ae72
Mauricio Messa <messammauricio@gmail.com>
2024-11-29 00:08:20 -0300

    12,13,14

---
+++ competitiveProgrammationC#/13_area/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+*Escreva um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. 
+*Em seguida, calcule e mostre:
+*a) a área do triângulo retângulo que tem A por base e C por altura.
+*b) a área do círculo de raio C. (pi = 3.14159)
+*c) a área do trapézio que tem A e B por bases e C por altura.
+*d) a área do quadrado que tem lado B.
+*e) a área do retângulo que tem lados A e B.
+*
+*Entrada
+*O arquivo de entrada contém três valores com um dígito após o ponto decimal.
+*
+*Saída
+*O arquivo de saída deverá conter 5 linhas de dados. Cada linha corresponde a uma das áreas 
+*descritas acima, sempre com mensagem correspondente e um espaço entre os dois pontos e o 
+*valor. O valor calculado deve ser apresentado com 3 dígitos após o ponto decimal.
+*/
+
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        string[] input = Console.ReadLine().Split(' ');
+        double A = double.Parse(input[0]);
+        double B = double.Parse(input[1]);
+        double C = double.Parse(input[2]);
+        double pi = 3.14159;
+        
+        double areaTri = A * C / 2;
+        double areaCir = pi * Math.Pow(C, 2);
+        double trap = (A + B) * C / 2;
+        double areaQuad = B * B;
+        double areaRetan = A * B;
+        
+        Console.WriteLine($"TRIANGULO: {areaTri:F3}");
+        Console.WriteLine($"CIRCULO: {areaCir:F3}");
+        Console.WriteLine($"TRAPEZIO: {trap:F3}");
+        Console.WriteLine($"QUADRADO: {areaQuad:F3}");
+        Console.WriteLine($"RETANGULO: {areaRetan:F3}");
+    }
+
+}
---
+++ competitiveProgrammationC#/14_oMaior/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+*Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da 
+*mensagem “eh o maior”. Utilize a fórmula:
+*(a+b+abs(a-b)) / 2
+*Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo,
+*portanto é necessário para chegar no resultado esperado.
+*
+*Entrada
+*O arquivo de entrada contém três valores inteiros.
+*
+*Saída
+*Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+*/
+
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        string[] input = Console.ReadLine().Split(' ');
+        int n1 = int.Parse(input[0]);
+        int n2 = int.Parse(input[1]);
+        int n3 = int.Parse(input[2]);
+        
+        int maiorAB = (n1 + n2 + Math.Abs(n1 - n2)) / 2;
+        int maior = (maiorAB + n3 + Math.Abs(maiorAB - n3)) / 2;
+        
+        Console.WriteLine($"{maior} eh o maior");
+    }
+
+}
