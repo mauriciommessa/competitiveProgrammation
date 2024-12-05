58914ab5171302d1452a7c3f9425ffb90258f211
Mauricio Messa <messammauricio@gmail.com>
2024-12-04 23:53:13 -0300

    conversaoDeTempo | idadeEmDias
    começando a queimar neuronios

---
+++ competitiveProgrammationC#/20_conversaoDeTempo/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma 
+fábrica, e informe-o expresso no formato horas:minutos:segundos.
+
+Entrada
+O arquivo de entrada contém um valor inteiro N.
+
+Saída
+Imprima o tempo lido no arquivo de entrada (segundos), convertido para 
+horas:minutos:segundos, conforme exemplo fornecido.
+*/
+
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        int segundos = int.Parse(Console.ReadLine());
+        
+        int min = segundos / 60;
+        int horas = min / 60;
+        min = min % 60;
+        int seg = segundos - (min * 60) - (horas * 60 * 60);
+
+        Console.WriteLine($"{horas}:{min}:{seg}");
+    }
+
+}
---
+++ competitiveProgrammationC#/21_idadeEmDias/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses 
+e dias
+
+Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 
+dias. Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 
+360, 363 ou 364. Este é apenas um exercício com objetivo de testar raciocínio matemático 
+simples.
+
+Entrada
+O arquivo de entrada contém um valor inteiro.
+
+Saída
+Imprima a saída conforme exemplo fornecido.
+*/
+
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        int dias = int.Parse(Console.ReadLine());
+
+        int ano = dias / 365;
+        int resto = dias % 365;
+        int mes = resto / 30;
+        int dia = resto % 30; 
+
+        Console.WriteLine($"{ano} ano(s)\n{mes} mes(es)\n{dia} dia(s)");
+    }
+
+}
